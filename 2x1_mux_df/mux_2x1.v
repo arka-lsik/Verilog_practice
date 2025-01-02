580f7da5b7abe6ac60525f13797b2957e886f104
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-03 01:09:10 +0530

    Create mux_2x1.v

---
+++ 2x1_mux_df/mux_2x1.v
@@ -0,0 +1,22 @@
+//2x1 mux data flow model.
+module mux_2x1_df(y,T,S);
+  input [1:0]I;
+  input s;
+  output Y;
+
+  assign y = s ? I[0] : I{1];
+endmodule                 
+
+//2x1 mux behavioral model
+    module mux_2x1_bh(y,I,S)
+       input [1:0]I;
+       input s;
+       output reg y;
+
+              always@(*)
+                  if(s)
+                    y = I[1];
+                  else 
+                    y = I[0];
+     endmodule
+                          
