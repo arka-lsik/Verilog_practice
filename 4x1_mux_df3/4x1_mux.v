451535db009fc503065a228a0d7255733ad4cb49
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-04 11:04:51 +0530

    Create 4x1_mux.v

---
+++ 4x1_mux_df3/4x1_mux.v
@@ -0,0 +1,9 @@
+// 4x1 mux dataflow model 3
+// This is the case based 4x1 mux design
+module 41_mux_df2(y,S,I);
+  input [3:0]I;
+  input [1:0]S;
+  output y;
+
+  assign y = S(2'd00) ? I[0] : (S(2'd01) ? I[1] : (S(2'd10) ? I[2] : I[3] ));
+endmodule
