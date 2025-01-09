b5a13e71e2ba16da72df4e3f2492446fce659f7e
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-09 12:46:07 +0530

    Create FA_structural.v

---
+++ Full Adder/FA_structural.v
@@ -0,0 +1,12 @@
+//Structural modeling of 1bit full adder
+module fa_st(a,b,cin,cout,sum);
+  input a,b,cin;
+  output cout,sum;
+  wire w1,w2,w3;
+
+  xor G1(w1,a,b);
+  xor G2(sum,w1,cin);
+  and G3(w2,a,b);
+  and G4(w3,w1,cin);
+  xor(cout,w3,w2);
+endmodule
