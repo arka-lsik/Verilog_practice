09ac3df0a132844a4316219ce476df72609a4e26
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-10 15:49:30 +0530

    Create 4b_fa_bh.v

---
+++ 4-bit Full Adder/4b_fa_bh.v
@@ -0,0 +1,9 @@
+//Behavioral model 4 bit full Adder
+module 4_fa_bh(a,b,cin,sum,cout);
+  input [3:0]a,b;
+  input cin;
+  output [3:0]sum;
+  output cout;
+
+  assign {sum,cout} = a+b+cin;
+endmodule
