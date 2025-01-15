1912b40b89389cff21ad0b8814ca8c5c74d4e099
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-15 11:56:53 +0530

    Create d_latch_rst_bh.v

---
+++ D Latch/d_latch_rst_bh.v
@@ -0,0 +1,11 @@
+//D latch behavioral model with Ashynchronus resst 
+module d_latch_rst_bh(q,d,en,rst);
+  input d,en,rst;
+  output q;
+
+  always@(d,en,rst)
+    if(rst)
+      q = 1'b0;
+  else if(en)
+    q =  d;
+endmodule
