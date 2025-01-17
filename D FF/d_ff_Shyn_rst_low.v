948eae0c352c06ed404711e553557ddc81dc8d29
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-17 13:07:15 +0530

    Update d_ff_Shyn_rst_low.v

--- D FF/d_ff_Shyn_rst_low.v
+++ D FF/d_ff_Shyn_rst_low.v
@@ -10,29 +10,3 @@ module (q,d,clk,rst);
     else 
       q<=d;
 endmodule
-
-//D ff shynchronus reset Active High
-module (q,d,clk,rst)
-  input d,clk,rst;
-  output q;
-
-  always@(posedge clk or posedge rst)
-    if(rst)
-      q<=1'b0;
-    else
-      q<=d;
-endmodule
-
-//D ff Shynchronus reset Active High & Low reset
-module (q,d,clk,rst)
-  input d,clk,rst;
-  output q;
-
-  always@(posedge clk or negedge rst or negedge rst)
-    if(!rst)
-      q<=1'b0;
-  else if(rst)
-      q<=1'b1;;
-  else 
-      q<=d;
-endmodule
