dd04c39e8321b7b6c3e32a03c16d56222c70a551
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-18 22:20:18 +0530

    Create shift_left.v

---
+++ Shift Register/shift_left.v
@@ -0,0 +1,23 @@
+//5 bit left shift register
+module (SI,SR,clk,rst,so);
+  input SI,clk,rst;
+  output so;
+  output reg[4:0]sr;
+
+  always@(posedge clk or negedge rst)
+    if(!rst)
+      begin
+        so<=5'b0; //to reset all FF
+        sr<=1'b0; //to reset 0th FF
+      end
+  else 
+    begin
+    sr[0]<=so;
+    sr[1]<=sr[0];
+    sr[2]<=sr[1];
+    sr[3]<=sr[2];
+    sr[4]<=sr[3];
+    so<=sr[4];
+    end
+endmodule
+  
