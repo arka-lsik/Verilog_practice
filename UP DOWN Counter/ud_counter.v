b7c1fd468b8de925ff2397660a6d0b42ffe90b52
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-01-23 12:33:16 +0530

    Create ud_counter.v

---
+++ UP DOWN Counter/ud_counter.v
@@ -0,0 +1,18 @@
+//Design a 8 Bit counter with UP & DOWN in same design.
+module (clk,rst,data,load,ud,count);
+  input clk,rst,load,ud;
+  output [7:0]count;
+  reg [7:0]count_temp;
+
+  always@(posedge clk or negedge rst)
+    if(!rst)
+      count_temp<=8'd0;
+  else if(load)
+      count_temp<=data;
+  else if(ud)
+      count_temp<=count_temp+1;
+  else
+      count_temp<=count_temp-1;
+
+  assign count=count_temp;
+endmodule
