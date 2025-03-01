a16bb32f4c91b32a0d89354d7fb7115dd0d4161d
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-03-01 12:39:55 +0530

    Create freq_div_4.v

---
+++ Frequency Divider/freq_div_4.v
@@ -0,0 +1,21 @@
+//frequency divider by 4
+module freq_div_4(clk_out,clk,rst);
+  input clk,rst;
+  output reg clk_out;
+  reg clk_div2;
+
+  always@(posedge clk or negedge rst)
+    begin
+      if(rst)
+        clk_div2<=1'b0;
+      else
+        clk_div2<=~clk_div2;
+    end
+  always@(posedge clk_div2 or negedge rst)
+    begin
+      if(rst)
+        clk_out<=1'b0;
+      else
+        clk_out<=~clk_out;
+    end
+endmodule
