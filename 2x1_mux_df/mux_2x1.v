d9f892b81e01864245effbfc98fd040a2c117d71
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-03-08 17:31:55 +0530

    Update mux_2x1.v

--- 2x1_mux_df/mux_2x1.v
+++ 2x1_mux_df/mux_2x1.v
@@ -1,22 +1,22 @@
 //2x1 mux data flow model.
-module mux_2x1_df(y,I,S);
+module mux_2x1_df(Y,I,S);
   input [1:0]I;
   input S;
-  output y;
+  output Y;
 
-  assign y = s ? I[0] : I[1];
+  assign Y = S ? I[0] : I[1];
 endmodule                 
 
 //2x1 mux behavioral model
-    module mux_2x1_bh(y,I,S)
+module mux_2x1_bh(Y,I,S)
        input [1:0]I;
-       input s;
-       output reg y;
+       input S;
+       output reg Y;
 
               always@(*)
-                  if(s)
-                    y = I[1];
+                if(S)
+                    Y = I[1];
                   else 
-                    y = I[0];
+                    Y = I[0];
      endmodule
                           
