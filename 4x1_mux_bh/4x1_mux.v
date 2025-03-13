fb7b42b7bb7c7d01cb6f73058ea62bff4fb952bc
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-03-13 10:51:30 +0530

    Update 4x1_mux.v

--- 4x1_mux_bh/4x1_mux.v
+++ 4x1_mux_bh/4x1_mux.v
@@ -1,11 +1,11 @@
 //4x1 mux code fo behavriol design
-module 41_mux_bh(y,S,I)
+module mux_4x1_bh(y,S,I);
   input [3:0]I;
   input [1:0]S;
   output reg y;
 
   always@(*)
-    case(s)
+    case(S)
       2'd0: y=I[0];
       2'd1: y=I[1];
       2'd2: y=I[2];
