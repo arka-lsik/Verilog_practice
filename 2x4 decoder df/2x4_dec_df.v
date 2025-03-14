b5b7fde03769ef298c60158c29558cd6574b35d6
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-03-14 10:57:04 +0530

    Update 2x4_dec_df.v

--- 2x4 decoder df/2x4_dec_df.v
+++ 2x4 decoder df/2x4_dec_df.v
@@ -1,5 +1,5 @@
 //Data flow design of 2x4 decoder 
-module 24_dec_df(I,En,y);
+module dec_2x4_df(I,En,y);
   input [1:0]I;
   input En;
   output [3:0]y;
@@ -7,5 +7,5 @@ module 24_dec_df(I,En,y);
   assign y = { En & I[0] & I[1] |
               En & I[0] & ~I[1] |
               En & ~I[0] & I[1] |
-              en & ~I[0] & I[1] };
+              en & ~I[0] & ~I[1] };
 endmodule
