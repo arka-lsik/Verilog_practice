089f0ffd5861d891da6691178ed88d89d0a5ed3d
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-08-25 02:24:51 +0530

    4b_fa_bh.v

--- 4-bit Full Adder/4b_fa_bh.v
+++ 4-bit Full Adder/4b_fa_bh.v
@@ -2,8 +2,8 @@
 module 4_fa_bh(a,b,cin,sum,cout);
   input [3:0]a,b;
   input cin;
-  output [3:0]sum;
-  output cout;
+  output reg [3:0]sum;
+  output reg cout;
 
   always@(*)
       {sum,cout} = a+b+cin;
